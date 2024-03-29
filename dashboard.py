4893f9aa8cbfced21df20d2f271700d47be3636b
Sergii Kotenko <s.kotenko@steel.mil.ua>
2024-03-29 17:20:59 +0200

    Code clean-up

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,6 @@ from pymongo import MongoClient
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Initialize connection to MongoDB (replace 'your_connection_string' with your actual connection string)
 try:
     conn = MongoClient("mongodb://monitoring:monitoring@127.0.0.1:10017/monitoring?authSource=monitoring&retryWrites=true&w=majority")
     print(conn)
@@ -14,20 +13,15 @@ except ConnectionError as e:
 db = conn.monitoring
 collection = db.records
 
-# Fetch data from MongoDB
 data = list(collection.find())
 
-# Convert the data into a Pandas DataFrame
 df = pd.DataFrame(data)
 
-# Remove the '_id' column
 df.drop('_id', axis=1, inplace=True)
 
 grouped = df.groupby('hostname')
 
-# Iterate over each group
 for name, group in grouped:
-    # Plotting the chart for each server
     fig, ax = plt.subplots()
     ax.plot(group.index, group['cpu'], label='CPU Usage (%)')
     ax.plot(group.index, group['ram'], label='RAM Usage (%)')
@@ -40,5 +34,4 @@ for name, group in grouped:
     ax.set_title(f'Server Utilization Over Time: {name}')
     ax.legend()
 
-    # Display the chart in Streamlit
     st.pyplot(fig)
